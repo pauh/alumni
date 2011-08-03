6b257cbb9ca048ae406a4bbda4c5ae35ee200a8f
Pau Haro Negre <pauharo@gmail.com>
2011-08-03 20:22:26 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alumni::Application.config.secret_token = 'cb88be162d497b4b6fa530bc4a4eb49c620aa5bb6c2f841d34e06dd2ae92dfe7bb6bd8310e62b36cb3c5774ece2292f1e2626bb116522a2a46f974c2594d06b5'
